db23ace2cd5f8390904e5ec9d152fc7802fa049b
Nurul_Amin <106983417+Nurul2Amin@users.noreply.github.com>
2026-02-08 00:44:12 +0600

    Update env.dart

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -4,6 +4,6 @@ class Env {
 
   
   // TODO: Replace with your actual Supabase URL and Key
-  static const String supabaseUrl = 'https://nyfituzqndzszydydnee.supabase.co';
-  static const String supabaseAnonKey = 'sb_publishable_hi4-cZIzUasJrrtlUMSiOw_QXsnKPUR';
+  static const String supabaseUrl = '';
+  static const String supabaseAnonKey = '';
 }
